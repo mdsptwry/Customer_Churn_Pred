017e156d747492c6fcf167581e517aec7840d384
mpshohan <mdpatwary377@gmail.com>
2024-09-09 17:48:53 -0400

    added ping.py so that app pings itself and stays active, and doesn't sleep due to inactivity

---
+++ ping.py
@@ -0,0 +1,20 @@
+import requests
+import time
+
+def ping_app():
+    web_url = "https://customerchurnpred-app.streamlit.app/"
+    while True:
+        try:
+            response = requests.get(web_url)
+            if response.status_code == 200:
+                print(f"Pring successful: {response.status_code}")
+            else:
+                print(f"Ping failed: {response.status_code}")
+        except Exception as e:
+            print(f"Error pinging the app:{e}")
+        
+        # wait for 10 mins (600 seconds) before pinging again
+        time.sleep(600)
+    
+if __name__=="__main__":
+    ping_app()
